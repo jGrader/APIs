f9434ae3ff5d9f730257cf27476cfd2b8c9b3d7c
ABarbarosie <alex_br1993@yahoo.com>
2014-11-22 06:02:30 +0200

    Added shhkey interface

---
+++ GraderDataAccessLayer/Interfaces/ISshKeyRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace GraderDataAccessLayer.Interfaces
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using GraderDataAccessLayer.Models;
+
+    interface ISshKeyRepository
+    {
+        IEnumerable<SshKeyModel> GetAll();
+        SshKeyModel Get(int id);
+        string GetKeyById(int id);
+        string GetKeyByUserId(int id);
+        bool Add(SshKeyModel item);
+        bool Remove(int id);
+        bool Update(SshKeyModel item);
+    }
+}
