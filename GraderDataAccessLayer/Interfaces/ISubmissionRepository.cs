e164dfea1146ea67218a207b3b337818c0de393c
ABarbarosie <alex_br1993@yahoo.com>
2014-11-22 06:02:47 +0200

    added submission interface

---
+++ GraderDataAccessLayer/Interfaces/ISubmissionRepository.cs
@@ -0,0 +1,28 @@
+﻿namespace GraderDataAccessLayer.Interfaces
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using GraderDataAccessLayer.Models;
+
+    interface ISubmissionRepository
+    {
+        IEnumerable<SubmissionModel> GetAll();
+        IEnumerable<SubmissionModel> GetByEntityId(int id);
+        IEnumerable<SubmissionModel> GetByUserId(int id);
+        IEnumerable<SubmissionModel> GetBefore(string timestamp);
+        IEnumerable<SubmissionModel> GetAfter(string timestamp);
+        SubmissionModel Get(int id);
+        IEnumerable<SubmissionModel> GetByUserIdAndEntityId(int userId, int entityId);
+        bool Add(SubmissionModel item);
+        bool Remove(int id);
+        bool Update(SubmissionModel item);
+        int GetUserId(int id);
+        int GetEntityId(int id);
+        string GetFileName(int id);
+        string GetFilePath(int id);
+        string GetTimeStamp(int id);
+    }
+}
