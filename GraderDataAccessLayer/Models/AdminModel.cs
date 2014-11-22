2b6e25018d9f0f1f22ce61f1556eda0bd5922cf3
ABarbarosie <alex_br1993@yahoo.com>
2014-11-22 03:13:31 +0200

    Added AdminModel

---
+++ GraderDataAccessLayer/Models/AdminModel.cs
@@ -0,0 +1,22 @@
+﻿namespace GraderDataAccessLayer.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    class AdminModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        //Navigation property
+        public int UserId { get; set; }
+        [Required]
+        [ForeignKey("UserId")]
+        public virtual UserModel Id { get; set; }
+
+        [Required]
+        public bool IsSuperUser { get; set; }
+    }
+}
