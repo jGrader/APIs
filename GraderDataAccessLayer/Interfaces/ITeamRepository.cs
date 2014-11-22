d17db3a2396498e247b7eda765e8ebfce6b26211
ABarbarosie <alex_br1993@yahoo.com>
2014-11-22 06:03:31 +0200

    added team interface

---
+++ GraderDataAccessLayer/Interfaces/ITeamRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace GraderDataAccessLayer.Interfaces
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using GraderDataAccessLayer.Models;
+
+    interface ITeamRepository
+    {
+        IEnumerable<TeamModel> GetAll();
+        TeamModel Get(int id);
+        bool Add(TeamModel item);
+        bool Remove(int id);
+        bool Update(TeamModel item);
+    }
+}
