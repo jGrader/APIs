7ae7183d7998fc22e0755f60cf6c32192dea0e8e
filipbitola <filipstankovski@yahoo.com>
2014-11-22 01:34:55 +0100

    testing

---
+++ GraderDataAccessLayer/Models/CourseModel.cs
@@ -0,0 +1,46 @@
+﻿
+namespace GraderDataAccessLayer.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data;
+    using System.Data.Entity.Infrastructure;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class Course
+    {
+        //Scalar Properties
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string CourseNumber { get; set; }
+
+        [DataType(DataType.Text)]
+        public string ShortName { get; set; }
+
+        public int Semester { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime StarTime { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime EndTime { get; set; }
+
+        //Navigation properties
+        public int OwnerId { get; set; }
+
+        /*
+        [ForeignKey("OwnerId")]
+        public virtual gawga Owner { get; set; }
+        */
+    }
+}
