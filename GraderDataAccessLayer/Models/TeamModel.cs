d14c5d525c792ce8c489b7402632ed24eb8221f7
ABarbarosie <alex_br1993@yahoo.com>
2014-11-22 03:14:13 +0200

    Added TeamModel

---
+++ GraderDataAccessLayer/Models/TeamModel.cs
@@ -0,0 +1,19 @@
+﻿namespace GraderDataAccessLayer.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    class TeamModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        //Navigation property
+        public int CourseId { get; set; }
+        [Required]
+        [ForeignKey("CourseId")]
+        public virtual CourseModel Id { get; set; }
+    }
+}
