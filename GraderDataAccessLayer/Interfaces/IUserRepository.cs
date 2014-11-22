ec34a10c542b74afa33bd21864e327a091327aa0
ABarbarosie <alex_br1993@yahoo.com>
2014-11-22 06:04:14 +0200

    added user interface

---
+++ GraderDataAccessLayer/Interfaces/IUserRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace GraderDataAccessLayer.Interfaces
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using GraderDataAccessLayer.Models;
+
+    interface IUserRepository
+    {
+        IEnumerable<UserModel> GetAll();
+        IEnumerable<UserModel> GetAllByGraduationYear(int year);
+        UserModel Get(int id);
+        string GetEmail(int id);
+        bool Add(UserModel item);
+        bool Remove(int id);
+        bool Update(UserModel item);
+    }
+}
