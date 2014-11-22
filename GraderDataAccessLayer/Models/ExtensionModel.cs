93d12272beb4f9b4f20ddefaa1768cadece2c4c9
ABarbarosie <alex_br1993@yahoo.com>
2014-11-22 03:13:46 +0200

    Added extensionModel

---
+++ GraderDataAccessLayer/Models/ExtensionModel.cs
@@ -0,0 +1,25 @@
+﻿namespace GraderDataAccessLayer.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    class ExtensionModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        //Navigation property
+        public int UserId { get; set; }
+        [Required]
+        [ForeignKey("UserId")]
+        public virtual UserModel Id { get; set; }
+
+        //Navigation property
+        public int EntityId { get; set; }
+        [Required]
+        [ForeignKey("EntityId")]
+        public virtual EntityModel Id { get; set; }
+    }
+}
