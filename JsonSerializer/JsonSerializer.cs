7a01aafe9869b20423bb51f21c4c2f004e04a63b
filipbitola <filipstankovski@yahoo.com>
2014-11-22 04:56:26 +0100

    Adding JSON Serializer class and minor fixes

--- JsonSerializer/JsonSerializer.cs
+++ JsonSerializer/JsonSerializer.cs
@@ -7,9 +7,24 @@
     using System.Threading.Tasks;
     using Newtonsoft.Json;
     using Newtonsoft.Json.Linq;
+    using GraderDataAccessLayer.Models;
 
-    public class JsonSerializer
+    public static class JsonSerializer
     {
-        
+        public static JObject ToJson(this CourseModel cm)
+        {
+            var result = new JObject();
+
+            result.Add("Id", cm.Id);
+            result.Add("Name", cm.Name);
+            result.Add("ShortName", cm.ShortName);
+            result.Add("CourseNumber", cm.CourseNumber);
+            result.Add("Semester", cm.Semester);
+            result.Add("StartTime", cm.StartTime);
+            result.Add("EndTime", cm.EndTime);
+            result.Add("OwnerId", cm.OwnerId);
+
+            return result;
+        }
     }
 }
