32c0b3dd33a5d5f7121a038a5ff721dc970c1b48
ABarbarosie <alex_br1993@yahoo.com>
2014-11-22 02:43:56 +0200

    AddedTaskModel

---
+++ GraderDataAccessLayer/Models/TaskModel.cs
@@ -0,0 +1,29 @@
+﻿namespace GraderDataAccessLayer.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    class TaskModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string Name { get; set; }
+
+        //Navigation property
+        public int GradeComponentId { get; set; }
+        [Required]
+        [ForeignKey("GradeComponentId")]
+        public virtual GradeComponentModel Id { get; set; }
+
+        //Navigation property
+        public int CourseId { get; set; }
+        [Required]
+        [ForeignKey("CourseId")]
+        public virtual Course Id { get; set; }
+    }
+}
