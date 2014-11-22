bfc52c5bb911a3ef77ab3e08a0dfc50a137d9521
filipbitola <filipstankovski@yahoo.com>
2014-11-22 04:20:31 +0100

    Trying again

---
+++ Services.cs/Logger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class Logger
+    {
+        private readonly string _pathName;
+
+        public Logger()
+        {
+            _pathName = ConfigurationManager.AppSettings["pathName"];
+        }
+        public void Log(Exception e)
+        {
+            using (var s = File.AppendText(String.Format(_pathName, DateTime.Now.ToShortDateString())))
+            {
+                s.Write(DateTime.Now.ToLocalTime());
+            }
+        }
+    }
+}
