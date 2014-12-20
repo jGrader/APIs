ae3d1075db51e1aaa065a678a201b27f4575a0cd
filipbitola <filipstankovski@yahoo.com>
2014-12-20 16:50:34 +0100

    BlaBla

---
+++ GraderApi/CoursePermissions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GraderApi
+{
+    public enum CoursePermissions
+    {
+        Nothing = 0,
+        CanSeeGrades = 1,
+        CanCreateTasks = 1<<1,
+        CanGrade = 1<<2,
+        CanUploadForOthers = 1<<3,
+        CanGrantExtensions = 1<<4,
+        CanGrantExcuses = 1<<5,
+        CanSeeFullGrades = 1<<6,
+
+    }
+}
