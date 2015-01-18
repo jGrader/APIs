8a2b8c441e69fb5be4ea2b8b631a063f35ab95a3
filipbitola <filipstankovski@yahoo.com>
2015-01-18 13:48:01 +0100

    Added logout function, renamed login

--- GraderApi/Controllers/UsersController.cs
+++ GraderApi/Controllers/UsersController.cs
@@ -1,8 +1,10 @@
 ﻿namespace GraderApi.Controllers
 {
+    using System.Web;
     using Filters;
     using Grader.JsonSerializer;
     using GraderDataAccessLayer;
+    using GraderDataAccessLayer.Models;
     using Services;
     using System;
     using System.Net;
@@ -14,11 +16,26 @@
     {
         private readonly UnitOfWork _unitOfWork;
         private readonly Logger _logger;
+        private readonly UserModel _user;
 
         public UsersController(UnitOfWork unitOfWork, Logger log)
         {
             _logger = log;
             _unitOfWork = unitOfWork;
+
+            if (HttpContext.Current == null)
+            {
+                _logger.Log(new Exception("The HTTP context was null in UsersController"));
+                throw new NullReferenceException("HttpContext.Current");
+            }
+
+            var tmp = HttpContext.Current.User as UserPrincipal;
+            if (tmp == null)
+            {
+                _logger.Log(new Exception("No principal was set."));
+                throw new NullReferenceException("UserPrincipal");
+            }
+            _user = tmp.User;
         }
 
         //GET: api/Users/All
@@ -38,12 +55,24 @@
             }
         }
 
-        // POST: api/Users/Authentication
+        // POST: api/Users/Login
         // Dummy action for Basic Authentication to call and let the Handler validate
         [HttpPost]
-        public HttpResponseMessage Authentication()
+        public HttpResponseMessage Login()
         {
             return Request.CreateResponse(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Users/Logout
+        [HttpPost]
+        public async Task<HttpResponseMessage> Logout()
+        {
+            var result = await _unitOfWork.SessionIdRepository.Delete(_user.Id);
+            if (result)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            return Request.CreateResponse(HttpStatusCode.InternalServerError);
         }
 
         // DELETE: api/Users/{userId}
